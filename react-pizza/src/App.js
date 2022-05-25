c1bdad38641346512859d73e0b7dde83e7a5dc68
stts <it.spectre.ru@gmail.com>
2022-05-25 19:49:27 +0500

    #4 Создаем компонент пицц и категории, рендер списка, spread-оператор

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -4,6 +4,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzaItems from './assets/pizza.json';
+
 import './scss/app.scss';
 
 function App() {
@@ -18,11 +20,15 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Мексиканская" price={500} />
-            <PizzaBlock title="Мексиканская2" price={1500} />
-            <PizzaBlock title="Мексиканская3" price={2500} />
-            <PizzaBlock title="Мексиканская4" price={3500} />
-            <PizzaBlock title="Мексиканская5" price={4500} />
+            {pizzaItems.map((obj) => (
+              <PizzaBlock
+                title={obj.title}
+                price={obj.price}
+                image={obj.imageUrl}
+                sizes={obj.sizes}
+                types={obj.types}
+              />
+            ))}
           </div>
         </div>
       </div>
