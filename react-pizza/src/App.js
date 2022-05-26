6f89a9c3aa04a1ed5078a504257c6ab916cc2689
stts <it.spectre.ru@gmail.com>
2022-05-26 22:12:05 +0500

    получаем пиццы с бэкенда (fetch), useEffect

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -4,11 +4,20 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzaItems from './assets/pizza.json';
-
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    // https://628f5eca0e69410599db3c53.mockapi.io/pizzasItems
+    fetch('https://628f5eca0e69410599db3c53.mockapi.io/pizzasItems')
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -20,8 +29,8 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzaItems.map((obj) => (
-              <PizzaBlock {...obj} />
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
         </div>
