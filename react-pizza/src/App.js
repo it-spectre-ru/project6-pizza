db7eb3ab4869881ea968b406011eeeda0fec3d56
stts <it.spectre.ru@gmail.com>
2022-05-27 18:53:18 +0500

    error 8 withou 7 part of

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -22,17 +22,15 @@ function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
-          </div>
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {items.map((obj) => (
+            <PizzaBlock key={obj.id} {...obj} />
+          ))}
         </div>
       </div>
     </div>
