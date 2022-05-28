c18889afba0785edda4f095723306a9b5c16ba74
stts <it.spectre.ru@gmail.com>
2022-05-28 23:55:25 +0500

    React Router -скелетон

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,36 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-
-  React.useEffect(() => {
-    // https://628f5eca0e69410599db3c53.mockapi.io/pizzasItems
-    fetch('https://628f5eca0e69410599db3c53.mockapi.io/pizzasItems')
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
-      });
-  }, []);
-
+  // const pathname = window.location.pathname;
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="content__top">
-          <Categories />
-          <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-          {items.map((obj) => (
-            <PizzaBlock key={obj.id} {...obj} />
-          ))}
+        <div className="container">
+          {/* {pathname === '/' && <Home />} */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ react-pizza/src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './NotFoundBlock.module.scss';
+
+const NotFoundBlock = () => {
+  return (
+    <div className={styles.root}>
+      <h1>
+        <span>😄</span>
+        <br />
+        Ничего не найдено
+      </h1>
+      <p className={styles.description}>Нет такой страницы</p>
+    </div>
+  );
+};
+
+export default NotFoundBlock;
