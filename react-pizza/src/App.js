1a4214af18b7214cd2f50f2faca549254f1211b5
stts <it.spectre.ru@gmail.com>
2022-06-06 19:31:31 +0500

    сделал redux хранилище, создал  добавил в реакт   пример работает

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
+
 import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
@@ -13,9 +16,20 @@ export const SearchContext = React.createContext();
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
 
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+      <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <span>{count}</span>
+      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+
+      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
           <Routes>
@@ -24,7 +38,7 @@ function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </div>
   );
 }
