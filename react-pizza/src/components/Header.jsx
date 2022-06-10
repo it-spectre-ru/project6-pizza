b8b5e3ef4132a1ab641be0e241831460d36929fb
stts <it.spectre.ru@gmail.com>
2022-06-10 19:05:09 +0500

    создаю REDUX логику для корзины v1

--- react-pizza/src/components/Header.jsx
+++ react-pizza/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 
+import { useSelector } from 'react-redux';
+
 import Search from './Search';
 import logoSvg from '../assets/img/pizza-logo.svg';
 
 function Header() {
+  const { items, totalPrice } = useSelector((state) => state.cart);
+
   return (
     <div className="header">
       <div className="container">
@@ -19,7 +23,7 @@ function Header() {
         <Search />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>{totalPrice}₽</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
@@ -49,7 +53,7 @@ function Header() {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>3</span>
+            <span>{items.length}</span>
           </Link>
         </div>
       </div>
