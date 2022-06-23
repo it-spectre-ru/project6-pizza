3ad36b14fbcfd6e78d5c46c8d13435aae43b6737
stts <it.spectre.ru@gmail.com>
2022-06-23 13:43:55 +0500

    add to json to localStorage

--- react-pizza/src/components/Header.tsx
+++ react-pizza/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
@@ -11,6 +12,11 @@ function Header() {
   const location = useLocation();
 
   const totalCount = items.reduce((sum: number, item: any) => sum + item.count, 0);
+
+  React.useEffect(() => {
+    const json = JSON.stringify(items);
+    localStorage.setItem('cart', json);
+  }, [items]);
 
   return (
     <div className="header">
